15e78749ab4dec1afc8293131312837e582b65ac
Rohinihiyal <rohinihiyal23@navgurukul.org>
2024-05-28 16:24:47 +0530

    Add files via upload

---
+++ n1.js
@@ -0,0 +1,311 @@
+// const input = require("readline-sync");
+// let n = input.questionInt("Enter any number: ");
+// let a = [];
+// let i = 0;
+// while(i<n)
+// {
+//     a[i]=input.questionInt("Enter input: ");
+//     i++;
+// }
+// i = n-1;
+// while(i>=0)
+// {
+//     console.log(a[i]);
+//     i--;
+// }
+
+
+// const input = require("readline-sync");
+// let n = input.questionInt("Enter any number: ");
+// let a = [];
+// let i = 0;
+// while(i<n)
+// {
+//     a[i]=input.questionInt("Enter input: ");
+//     i++;
+// }
+// console.log(a)
+
+
+// const input = require("readline-sync");
+// let n = input.questionInt("Enter any number: ");
+// let a = [];
+// let i = 0;
+// while(i < n)
+// {
+//     a[i]=input.questionInt("Enter any number: ");
+//     i++;
+// }
+// let s=0;
+// let w=0;
+// i = 0;
+// while(i<n)
+// {
+//     if(a[i]%2==0)
+//     {
+//         s = s + a[i];
+//     }
+//     else
+//     {
+//         w = w + a[i];
+//     }
+//     i++;
+// }
+// console.log(s)
+// console.log(w)
+
+
+// const input = require("readline-sync");
+// let n = input.questionInt("Enter number: ");
+// let a;
+// a=n;
+// console.log(a)
+
+
+
+
+// const input = require ("readline-sync");
+// let a = input.questionInt("Enter number:");
+// let b = input.questionInt("Enter number:");
+//  t = a 
+//  a = b
+//  b = t 
+//console.log(a,b)
+
+
+
+
+// const input = require("readline-sync");
+// let a = input.questionInt("enter number:");
+// let b = input.questionInt("enter number:");
+// A = a+b
+// S = a-b
+// M = a*b
+// D = a/b
+//console.log(A,S,M,D)
+
+
+
+
+
+// const input = require("readline-sync");
+// let a = input. questionInt("enter number:");
+// let b = input. questionInt("enter number:");
+// let q;
+// let S;
+// Q=a%b
+// S= Math.floor (a/b)
+// console.log(Q,S)
+
+
+
+
+// const input = require("readline-sync");
+// let n = input.questionInt("enter number:");
+// let p;
+// p = n%10
+// console.log(p)
+
+
+
+// const input = require("readline-sync");
+// let c = input.questionInt("enter number:");
+// let p ;
+// let f ;
+//  f = 9*c + 160 
+//  p = f/5
+// console.log(p)
+
+     
+
+// const input = require("readline-sync")
+// let a = input.questionInt("enter number: ")
+// let b = input.questionInt("enter number: ")
+// let p;
+// let s;
+// p = Math.floor(a/b)
+// s = b* p
+// console.log(s)
+
+
+
+// const input = require("readline-sync")
+// let a = input.questionInt("enter number :")
+// let b = input.questionInt("enter number: ")
+// if(a>b)
+// {
+//    console.log(a)
+// }
+// else
+// {
+//         console.log(b)
+// }
+
+
+
+
+// const input = require ("readline-sync")
+// let a = input.questionInt("enter number: ")
+// if(a%2==0)
+// {
+//         console.log("even")
+// }
+// else
+// {
+//         console.log("odd")
+
+// }
+
+
+
+
+// const input = require("readline-sync")
+// let a = input.questionInt("enter number: ")
+// if(a>0)
+// {
+//         console.log("positive")
+// }
+// if(a<0)
+// {
+//         console.log("negative")
+// }
+//  if(a==0)
+//  {
+//         console.log("zero")
+//  }
+
+
+// const input = require("readline-sync")
+// let a = input.questionInt("enter number: ")
+// let b = input.questionInt("enter number: ")
+// let c = input.questionInt("enter number: ")
+// if(a>b)
+// {
+//         if(a<c)
+//         {
+//                 console.log(c)
+//         }
+//         else
+//         {
+//                 console.log(a)
+//         }
+        
+        
+// }
+// else if(b>c)
+// {
+//         console.log(b)
+// }
+// else
+// {
+//         console.log(c)
+// }
+
+ 
+
+
+const input =require("readline-sync")
+let a = input.questionInt("enter number: ")
+let b = input.questionInt("enter number: ")
+let c = input.questionInt("enter number: ")
+let d = input.questionInt("enter number: ")
+if(a>b)
+{
+        if(a>c)
+        {
+                if(a>d)
+                {
+                        console.log(a)
+                }
+                else
+                {
+                 console.log(d)
+                }
+        }
+        else if (c>d)
+        {
+                consople.log(c)
+        }
+        else
+        {
+                console.log(d)
+        }
+}
+else if (b>c)
+{
+        if(b>d)
+        {
+                console.log(b)
+        }
+        else
+        {
+                console.log(d)
+        }
+}
+else if ()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
